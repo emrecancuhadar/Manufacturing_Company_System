630b0c2193bcf56d5f6149afe5418aafbd572909
Berk Şengül <berksengul17@gmail.com>
2025-04-20 17:42:46 +0300

    bug fix

--- src/InManufacturingState.java
+++ src/InManufacturingState.java
@@ -3,8 +3,9 @@ public class InManufacturingState implements ManufacturingState {
     @Override
     public void handle(ManufacturingProcess process, 
                         Storage storage, Blueprint blueprint, 
-                        Report report) {
+                        Report report) throws ManufacturingProcessNotValid, InvalidComponentNodeException, InvalidStorageException {
         ManufacturingState randState = RandomStateGenerator.generateRandomState();
         process.setCurrentState(randState);
+        process.getCurrentState().handle(process, storage, blueprint, report);
     }
 } 
